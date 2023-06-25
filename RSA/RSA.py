fb18b31c8de0a41c684da4fc8e1f951282d6335f
Manish-manku <119307013+Manish-manku@users.noreply.github.com>
2023-06-25 17:13:07 +0530

    Add files via upload

---
+++ RSA/RSA.py
@@ -0,0 +1,32 @@
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.hazmat.primitives import serialization, hashes
+
+# Generate RSA key pair
+private_key = rsa.generate_private_key(
+    public_exponent=65537,
+    key_size=2048
+)
+public_key = private_key.public_key()
+
+# Encryption
+message = input("Enter a text:").encode()
+ciphertext = public_key.encrypt(
+    message,
+    padding.OAEP(
+        mgf=padding.MGF1(algorithm=hashes.SHA256()),
+        algorithm=hashes.SHA256(),
+        label=None
+    )
+)
+print("Ciphertext:", ciphertext.hex())
+
+# Decryption
+decrypted_message = private_key.decrypt(
+    ciphertext,
+    padding.OAEP(
+        mgf=padding.MGF1(algorithm=hashes.SHA256()),
+        algorithm=hashes.SHA256(),
+        label=None
+    )
+)
+print("Decrypted message:", decrypted_message.decode())
